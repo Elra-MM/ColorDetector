a1a79ecbfe7311e9e338bf0192f55b6d843114bf
Morgane-Minsar <68017139+Morgane-Minsar@users.noreply.github.com>
2024-09-23 13:02:59 +0200

    Try to implement CIE2000 distance- bad color result

--- app/src/main/java/com/example/colordetector/ColorCalculator.java
+++ app/src/main/java/com/example/colordetector/ColorCalculator.java
@@ -1,6 +1,7 @@
 package com.example.colordetector;
 
 import android.content.res.AssetManager;
+import android.nfc.Tag;
 import android.util.Log;
 
 import org.opencv.core.Mat;
@@ -37,7 +38,7 @@ public class ColorCalculator {
     protected ColorCalculator(AssetManager assets) {
         mCIELab = new Mat();
         mediansColor = new ArrayList<>();
-        createColorSet(assets);
+        colorSetCIE = createColorSet(assets);
     }
 
     protected String getMedianName() {
@@ -61,7 +62,6 @@ public class ColorCalculator {
         if (scalars == null || scalars.isEmpty()) {
             throw new IllegalArgumentException("The list of scalars is null or empty.");
         }
-
         double L = scalars.stream().mapToDouble(scalar -> scalar.val[0]).average().orElse(0);
         double A = scalars.stream().mapToDouble(scalar -> scalar.val[1]).average().orElse(0);
         double B = scalars.stream().mapToDouble(scalar -> scalar.val[2]).average().orElse(0);
@@ -77,7 +77,7 @@ public class ColorCalculator {
     private HashMap<String, List<Double>> createColorSet(AssetManager assets) {
         HashMap<String, List<Double>> set = new HashMap<>();
         try {
-            InputStream inputStream = assets.open("colorset.csv");
+            InputStream inputStream = assets.open("colorsetCut.csv");
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
             reader.readLine(); // skip the first line
             String line;
@@ -136,23 +136,72 @@ public class ColorCalculator {
 
     private String getNameCIE(Scalar medianColor) {
         // Pareil ici, pour éviter le `int min = Integer.MAX_VALUE;` j'aurais fait stream() et min() ou équivalent
-        int min = Integer.MAX_VALUE;
+        double min = Integer.MAX_VALUE;
         String name = "";
-
 
         //TODO : can use stream() and colorSetCIE.entrySet()
         for (String key : colorSetCIE.keySet()) {
             List<Double> color = colorSetCIE.get(key);
-
-            //TODO : Calculate the CIEDE2000 distance between the two colors
-
-            //distance for RGB
-            int distance = (int) (Math.pow(color.get(0) - medianColor.val[0], 2) + Math.pow(color.get(1) - medianColor.val[1], 2) + Math.pow(color.get(2) - medianColor.val[2], 2));
+            Log.i(TAG, "color =" + color.get(0) + " " + color.get(1) + " " + color.get(2));
+            double distance = getDistanceCIE2000(medianColor, new Scalar(color.get(0), color.get(1), color.get(2)));
             if (distance < min) {
                 min = distance;
                 name = key;
             }
         }
         return name;
+    }
+
+    private double getDistanceCIE2000(Scalar color1, Scalar color2) {
+        double L1 = color1.val[0];
+        double a1 = color1.val[1];
+        double b1 = color1.val[2];
+        double L2 = color2.val[0];
+        double a2 = color2.val[1];
+        double b2 = color2.val[2];
+
+        double Lmean = (L1 + L2) / 2.0;
+        double C1 =  Math.sqrt(a1*a1 + b1*b1);
+        double C2 =  Math.sqrt(a2*a2 + b2*b2);
+        double Cmean = (C1 + C2) / 2.0;
+
+        double G =  ( 1 - Math.sqrt( Math.pow(Cmean, 7) / (Math.pow(Cmean, 7) + Math.pow(25, 7)) ) ) / 2; //ok
+        double a1prime = a1 * (1 + G);
+        double a2prime = a2 * (1 + G);
+
+        double C1prime =  Math.sqrt(a1prime*a1prime + b1*b1);
+        double C2prime =  Math.sqrt(a2prime*a2prime + b2*b2);
+        double Cmeanprime = (C1prime + C2prime) / 2;
+
+        double h1prime =  Math.atan2(b1, a1prime) + 2*Math.PI * (Math.atan2(b1, a1prime)<0 ? 1 : 0);
+        double h2prime =  Math.atan2(b2, a2prime) + 2*Math.PI * (Math.atan2(b2, a2prime)<0 ? 1 : 0);
+        double Hmeanprime =  ((Math.abs(h1prime - h2prime) > Math.PI) ? (h1prime + h2prime + 2*Math.PI) / 2 : (h1prime + h2prime) / 2);
+
+        double T =  1.0 - 0.17 * Math.cos(Hmeanprime - Math.PI/6.0) + 0.24 * Math.cos(2*Hmeanprime) + 0.32 * Math.cos(3*Hmeanprime + Math.PI/30) - 0.2 * Math.cos(4*Hmeanprime - 21*Math.PI/60);
+
+        double deltahprime =  ((Math.abs(h1prime - h2prime) <= Math.PI) ? h2prime - h1prime : (h2prime <= h1prime) ? h2prime - h1prime + 2*Math.PI : h2prime - h1prime - 2*Math.PI);
+
+        double deltaLprime = L2 - L1;
+        double deltaCprime = C2prime - C1prime;
+        double deltaHprime =  2.0 * Math.sqrt(C1prime*C2prime) * Math.sin(deltahprime / 2.0);
+        double SL =  1.0 + ( (0.015*(Lmean - 50)*(Lmean - 50)) / (Math.sqrt( 20 + (Lmean - 50)*(Lmean - 50) )) );
+        double SC =  1.0 + 0.045 * Cmeanprime;
+        double SH =  1.0 + 0.015 * Cmeanprime * T;
+
+        double deltaTheta =  (30 * Math.PI / 180) * Math.exp(-((180/Math.PI*Hmeanprime-275)/25)*((180/Math.PI*Hmeanprime-275)/25));
+        double RC =  (2 * Math.sqrt(Math.pow(Cmeanprime, 7) / (Math.pow(Cmeanprime, 7) + Math.pow(25, 7))));
+        double RT =  (-RC * Math.sin(2 * deltaTheta));
+
+        double KL = 1;
+        double KC = 1;
+        double KH = 1;
+
+        double deltaE = Math.sqrt(
+                ((deltaLprime/(KL*SL)) * (deltaLprime/(KL*SL))) +
+                        ((deltaCprime/(KC*SC)) * (deltaCprime/(KC*SC))) +
+                        ((deltaHprime/(KH*SH)) * (deltaHprime/(KH*SH))) +
+                        (RT * (deltaCprime/(KC*SC)) * (deltaHprime/(KH*SH)))
+        );
+        return deltaE;
     }
 }
