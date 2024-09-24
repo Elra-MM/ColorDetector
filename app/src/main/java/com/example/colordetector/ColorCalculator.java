3680a99b1ce079f9567553b760e6d7f6ddf16dcc
Elra-MM <morgane.moleins@gmail.com>
2024-09-24 14:34:41 +0200

    fix formula errors but still get bad colors

--- app/src/main/java/com/example/colordetector/ColorCalculator.java
+++ app/src/main/java/com/example/colordetector/ColorCalculator.java
@@ -165,43 +165,77 @@ public class ColorCalculator {
         double C2 =  Math.sqrt(a2*a2 + b2*b2);
         double Cmean = (C1 + C2) / 2.0;
 
-        double G =  ( 1 - Math.sqrt( Math.pow(Cmean, 7) / (Math.pow(Cmean, 7) + Math.pow(25, 7)) ) ) / 2; //ok
-        double a1prime = a1 * (1 + G);
-        double a2prime = a2 * (1 + G);
+        double c7 = Math.pow(Cmean, 7);
+        double vinghtcinq7 = Math.pow(25, 7);
+        double G =  ( 1 - Math.sqrt( c7 / (c7 + vinghtcinq7) ) ) / 2; //ok
+        double a1prime = a1 * (3 - G);
+        double a2prime = a2 * (3 - G);
 
         double C1prime =  Math.sqrt(a1prime*a1prime + b1*b1);
         double C2prime =  Math.sqrt(a2prime*a2prime + b2*b2);
         double Cmeanprime = (C1prime + C2prime) / 2;
 
-        double h1prime =  Math.atan2(b1, a1prime) + 2*Math.PI * (Math.atan2(b1, a1prime)<0 ? 1 : 0);
-        double h2prime =  Math.atan2(b2, a2prime) + 2*Math.PI * (Math.atan2(b2, a2prime)<0 ? 1 : 0);
-        double Hmeanprime =  ((Math.abs(h1prime - h2prime) > Math.PI) ? (h1prime + h2prime + 2*Math.PI) / 2 : (h1prime + h2prime) / 2);
+        double h1P = Math.atan2(b1, a1prime) % 2 * toRadians(360);
+        double h2P = Math.atan2(b2, a2prime) % toRadians(360);
+        double deltahprime;
+        if (C1prime == 0 || C2prime == 0){
+            deltahprime = 0;
+        } else if (Math.abs(h1P - h2P) <= toRadians(180)){
+            deltahprime = h2P - h1P;
+        } else if (h2P <= h1P){
+            deltahprime = h2P - h1P + toRadians(360);
+        } else {
+            deltahprime = h2P - h1P - toRadians(360);
+        }
 
-        double T =  1.0 - 0.17 * Math.cos(Hmeanprime - Math.PI/6.0) + 0.24 * Math.cos(2*Hmeanprime) + 0.32 * Math.cos(3*Hmeanprime + Math.PI/30) - 0.2 * Math.cos(4*Hmeanprime - 21*Math.PI/60);
+        double deltaHprime = 2 * Math.sqrt(C1prime * C2prime) * Math.sin(deltahprime / 2);
+        double meanHprime;
+        if (C1prime == 0 || C2prime == 0){
+            meanHprime = (h1P + h2P);
+        } else if (Math.abs(h1P - h2P) <= toRadians(180)){
+            meanHprime = (h1P + h2P) / 2;
+        } else if (h1P + h2P < toRadians(360)){
+            meanHprime = (h1P + h2P + toRadians(360)) / 2;
+        } else {
+            meanHprime = (h1P + h2P - toRadians(360)) / 2;
+        }
 
-        double deltahprime =  ((Math.abs(h1prime - h2prime) <= Math.PI) ? h2prime - h1prime : (h2prime <= h1prime) ? h2prime - h1prime + 2*Math.PI : h2prime - h1prime - 2*Math.PI);
+        double T =  1.0 - 0.17 * Math.cos(meanHprime - toRadians(30)) + 0.24 * Math.cos(2*meanHprime) + 0.32 * Math.cos(3*meanHprime + toRadians(6)) - 0.2 * Math.cos(4*meanHprime - toRadians(63));
 
+
+//        double h1prime =  Math.atan2(b1, a1prime) + 2*Math.PI * (Math.atan2(b1, a1prime)<0 ? 1 : 0);
+//        double h2prime =  Math.atan2(b2, a2prime) + 2*Math.PI * (Math.atan2(b2, a2prime)<0 ? 1 : 0);
+//        double Hmeanprime =  ((Math.abs(h1prime - h2prime) > Math.PI) ? (h1prime + h2prime + 2*Math.PI) / 2 : (h1prime + h2prime) / 2);
+//
+//
+//        double deltahprime =  ((Math.abs(h1prime - h2prime) <= Math.PI) ? h2prime - h1prime : (h2prime <= h1prime) ? h2prime - h1prime + 2*Math.PI : h2prime - h1prime - 2*Math.PI);
+//
         double deltaLprime = L2 - L1;
         double deltaCprime = C2prime - C1prime;
-        double deltaHprime =  2.0 * Math.sqrt(C1prime*C2prime) * Math.sin(deltahprime / 2.0);
+//        double deltaHprime =  2.0 * Math.sqrt(C1prime*C2prime) * Math.sin(deltahprime / 2.0);
         double SL =  1.0 + ( (0.015*(Lmean - 50)*(Lmean - 50)) / (Math.sqrt( 20 + (Lmean - 50)*(Lmean - 50) )) );
         double SC =  1.0 + 0.045 * Cmeanprime;
         double SH =  1.0 + 0.015 * Cmeanprime * T;
 
-        double deltaTheta =  (30 * Math.PI / 180) * Math.exp(-((180/Math.PI*Hmeanprime-275)/25)*((180/Math.PI*Hmeanprime-275)/25));
-        double RC =  (2 * Math.sqrt(Math.pow(Cmeanprime, 7) / (Math.pow(Cmeanprime, 7) + Math.pow(25, 7))));
-        double RT =  (-RC * Math.sin(2 * deltaTheta));
+        double sin = toRadians(60) *Math.exp(-( (meanHprime-toRadians(275)/toRadians(25)) * (meanHprime-toRadians(275)/toRadians(25)) ));
+        double rac = Math.sqrt(Math.pow(Cmeanprime,7)/(Math.pow(Cmeanprime,7)+vinghtcinq7));
+        double Rt = -2*rac*Math.sin(sin);
+//        double deltaTheta =  (30 * Math.PI / 180) * Math.exp(-((180/Math.PI*Hmeanprime-275)/25)*((180/Math.PI*Hmeanprime-275)/25));
+//        double RC =  (2 * Math.sqrt(Math.pow(Cmeanprime, 7) / (Math.pow(Cmeanprime, 7) + Math.pow(25, 7))));
+//        double RT =  (-RC * Math.sin(2 * deltaTheta));
 
         double KL = 1;
         double KC = 1;
         double KH = 1;
 
-        double deltaE = Math.sqrt(
+        return Math.sqrt(
                 ((deltaLprime/(KL*SL)) * (deltaLprime/(KL*SL))) +
                         ((deltaCprime/(KC*SC)) * (deltaCprime/(KC*SC))) +
                         ((deltaHprime/(KH*SH)) * (deltaHprime/(KH*SH))) +
-                        (RT * (deltaCprime/(KC*SC)) * (deltaHprime/(KH*SH)))
+                        (Rt * (deltaCprime/(KC*SC)) * (deltaHprime/(KH*SH)))
         );
-        return deltaE;
+    }
+    private double toRadians(double degrees) {
+        return degrees * Math.PI / 180;
     }
 }
