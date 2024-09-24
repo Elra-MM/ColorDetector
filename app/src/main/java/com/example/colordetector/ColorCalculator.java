1b63b9d7dd5981b3c4b914bd67fad08437c1193d
Elra-MM <morgane.moleins@gmail.com>
2024-09-24 14:34:42 +0200

    try multithreading

--- app/src/main/java/com/example/colordetector/ColorCalculator.java
+++ app/src/main/java/com/example/colordetector/ColorCalculator.java
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.Executors;
 
 import static org.opencv.imgproc.Imgproc.COLOR_RGB2HSV_FULL;
 import static org.opencv.imgproc.Imgproc.COLOR_RGB2Lab;
@@ -54,15 +55,20 @@ public class ColorCalculator {
             Log.e(TAG, "New frame is null or empty.");
             return;
         }
-        mediansColor.add(computeMedian(newRgba));
+        Executors.newWorkStealingPool().submit((() -> {
+            mediansColor.add(computeMedian(newRgba));
+        }));
+
     }
 
     protected void computeNewName() {
-        medianName = getNameCIE(computeAverage(mediansColor));
+        Executors.newWorkStealingPool().submit((() -> {
+            medianName = getNameCIE(computeAverage(mediansColor));
+        }));
         mediansColor.clear();
     }
 
-    protected Scalar computeAverage(List<Scalar> scalars) {
+    private Scalar computeAverage(List<Scalar> scalars) {
         if (scalars == null || scalars.isEmpty()) {
             throw new IllegalArgumentException("The list of scalars is null or empty.");
         }
@@ -155,7 +161,11 @@ public class ColorCalculator {
         return name;
     }
 
-    //TODO : refactor this method for perf
+    private final double k_L = 1.0, k_C = 1.0, k_H = 1.0;
+    private final double deg360InRad = toRadians(360.0);
+    private final double deg180InRad = toRadians(180.0);
+    private final double pow25To7 = Math.pow(25, 7);
+
     private double getDistanceCIE2000(Scalar color1, Scalar color2) {
         double l1 = color1.val[0];
         double a1 = color1.val[1];
@@ -164,28 +174,20 @@ public class ColorCalculator {
         double a2 = color2.val[1];
         double b2 = color2.val[2];
 
-        double k_L = 1.0, k_C = 1.0, k_H = 1.0;
-        double deg360InRad = toRadians(360.0);
-        double deg180InRad = toRadians(180.0);
-        double pow25To7 = 6103515625.0; /* Math.pow(25, 7) */
-
-        /*
-         * Step 1
-         */
-        /* Equation 2 */
         double C1 = Math.sqrt((a1 * a1) + (b1 * b1));
         double C2 = Math.sqrt((a2 * a2) + (b2 * b2));
-        /* Equation 3 */
-        double barC = (C1 + C2) / 2.0;
-        /* Equation 4 */
-        double G = 0.5 * (1 - Math.sqrt(Math.pow(barC, 7) / (Math.pow(barC, 7) + pow25To7)));
-        /* Equation 5 */
+
+        double meanC = (C1 + C2) * 0.5;
+
+        double meanCpow7 = Math.pow(meanC, 7);
+        double G = 0.5 * (1 - Math.sqrt(meanCpow7 / (meanCpow7 + pow25To7)));
+
         double a1Prime = (1.0 + G) * a1;
         double a2Prime = (1.0 + G) * a2;
-        /* Equation 6 */
-        double CPrime1 = Math.sqrt((a1Prime * a1Prime) + (b1 * b1));
-        double CPrime2 = Math.sqrt((a2Prime * a2Prime) + (b2 * b2));
-        /* Equation 7 */
+
+        double c1Prime = Math.sqrt((a1Prime * a1Prime) + (b1 * b1));
+        double c2Prime = Math.sqrt((a2Prime * a2Prime) + (b2 * b2));
+
         double hPrime1;
         if (b1 == 0 && a1Prime == 0)
             hPrime1 = 0.0;
@@ -211,73 +213,64 @@ public class ColorCalculator {
                 hPrime2 += deg360InRad;
         }
 
-        /*
-         * Step 2
-         */
-        /* Equation 8 */
         double deltaLPrime = l2 - l1;
-        /* Equation 9 */
-        double deltaCPrime = CPrime2 - CPrime1;
-        /* Equation 10 */
+
+        double deltaCPrime = c2Prime - c1Prime;
+
         double deltahPrime;
-        double CPrimeProduct = CPrime1 * CPrime2;
+        double CPrimeProduct = c1Prime * c2Prime;
         if (CPrimeProduct == 0)
             deltahPrime = 0;
         else {
-            /* Avoid the fabs() call */
             deltahPrime = hPrime2 - hPrime1;
             if (deltahPrime < -deg180InRad)
                 deltahPrime += deg360InRad;
             else if (deltahPrime > deg180InRad)
                 deltahPrime -= deg360InRad;
         }
-        /* Equation 11 */
-        double deltaHPrime = 2.0 * Math.sqrt(CPrimeProduct) *
-                Math.sin(deltahPrime / 2.0);
 
-        /*
-         * Step 3
-         */
-        /* Equation 12 */
-        double barLPrime = (l1 + l2) / 2.0;
-        /* Equation 13 */
-        double barCPrime = (CPrime1 + CPrime2) / 2.0;
-        /* Equation 14 */
-        double barhPrime, hPrimeSum = hPrime1 + hPrime2;
-        if (CPrime1 * CPrime2 == 0) {
-            barhPrime = hPrimeSum;
+        double deltaHPrime = 2.0 * Math.sqrt(CPrimeProduct) *
+                Math.sin(deltahPrime * 0.5);
+
+        double lMeanPrime = (l1 + l2) * 0.5;
+
+        double cMeanPrime = (c1Prime + c2Prime) * 0.5;
+
+        double hMeanPrime;
+        double hPrimeSum = hPrime1 + hPrime2;
+        if (c1Prime * c2Prime == 0) {
+            hMeanPrime = hPrimeSum;
         } else {
             if (Math.abs(hPrime1 - hPrime2) <= deg180InRad)
-                barhPrime = hPrimeSum / 2.0;
+                hMeanPrime = hPrimeSum * 0.5;
             else {
                 if (hPrimeSum < deg360InRad)
-                    barhPrime = (hPrimeSum + deg360InRad) / 2.0;
+                    hMeanPrime = (hPrimeSum + deg360InRad) * 0.5;
                 else
-                    barhPrime = (hPrimeSum - deg360InRad) / 2.0;
+                    hMeanPrime = (hPrimeSum - deg360InRad) * 0.5;
             }
         }
-        /* Equation 15 */
-        double T = 1.0 - (0.17 * Math.cos(barhPrime - toRadians(30.0))) +
-                (0.24 * Math.cos(2.0 * barhPrime)) +
-                (0.32 * Math.cos((3.0 * barhPrime) + toRadians(6.0))) -
-                (0.20 * Math.cos((4.0 * barhPrime) - toRadians(63.0)));
-        /* Equation 16 */
+
+        double T = 1.0 - (0.17 * Math.cos(hMeanPrime - toRadians(30.0))) +
+                (0.24 * Math.cos(2.0 * hMeanPrime)) +
+                (0.32 * Math.cos((3.0 * hMeanPrime) + toRadians(6.0))) -
+                (0.20 * Math.cos((4.0 * hMeanPrime) - toRadians(63.0)));
+
         double deltaTheta = toRadians(30.0) *
-                Math.exp(-Math.pow((barhPrime - toRadians(275.0)) / toRadians(25.0), 2.0));
-        /* Equation 17 */
-        double R_C = 2.0 * Math.sqrt(Math.pow(barCPrime, 7.0) /
-                (Math.pow(barCPrime, 7.0) + pow25To7));
-        /* Equation 18 */
-        double S_L = 1 + ((0.015 * Math.pow(barLPrime - 50.0, 2.0)) /
-                Math.sqrt(20 + Math.pow(barLPrime - 50.0, 2.0)));
-        /* Equation 19 */
-        double S_C = 1 + (0.045 * barCPrime);
-        /* Equation 20 */
-        double S_H = 1 + (0.015 * barCPrime * T);
-        /* Equation 21 */
+                Math.exp(-Math.pow((hMeanPrime - toRadians(275.0)) / toRadians(25.0), 2.0));
+
+        double R_C = 2.0 * Math.sqrt(Math.pow(cMeanPrime, 7.0) /
+                (Math.pow(cMeanPrime, 7.0) + pow25To7));
+
+        double S_L = 1 + ((0.015 * Math.pow(lMeanPrime - 50.0, 2.0)) /
+                Math.sqrt(20 + Math.pow(lMeanPrime - 50.0, 2.0)));
+
+        double S_C = 1 + (0.045 * cMeanPrime);
+
+        double S_H = 1 + (0.015 * cMeanPrime * T);
+
         double R_T = (-Math.sin(2.0 * deltaTheta)) * R_C;
 
-        /* Equation 22 */
         return Math.sqrt(
                 Math.pow(deltaLPrime / (k_L * S_L), 2.0) +
                         Math.pow(deltaCPrime / (k_C * S_C), 2.0) +
